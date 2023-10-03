650e4f248fcfbb08a485109f9799889999472619
pizzawarrior <i.norstad@gmail.com>
2023-10-02 17:37:18 -0700

    loaded database

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm
-from recipes.models import Recipe
+from recipes.models import Recipe, Ingredients, RecipeStep
 
 class RecipeForm(ModelForm):
     class Meta:
@@ -9,4 +9,20 @@ class RecipeForm(ModelForm):
             'picture',
             'description',
             'rating'
+        )
+
+class IngredientsForm(ModelForm):
+    class Meta:
+        model = Ingredients
+        fields = (
+            'food_item',
+            'amount'
+        )
+
+class RecipeStepForm(ModelForm):
+    class Meta:
+        model = RecipeStep
+        fields = (
+            'instruction',
+            'order'
         )
--- recipes/views.py
+++ recipes/views.py
@@ -1,28 +1,66 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from recipes.models import Recipe
-from recipes.forms import RecipeForm
+from recipes.forms import RecipeForm, IngredientsForm, RecipeStepForm
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
 
-#create
+#create recipe
 @login_required(login_url='/accounts/login/')
 def create_recipe(request):
     if request.method == 'POST':
-        form  = RecipeForm(request.POST)
-        if form.is_valid():
-            recipe = form.save(False)
-            recipe.author = request.user
-            recipe.save()
+        recipe_form  = RecipeForm(request.POST)
+        ingredients_form = IngredientsForm(request.POST)
+        recipe_step_form = RecipeStepForm(request.POST)
+        if all([recipe_form.is_valid() and ingredients_form.is_valid() and recipe_step_form.is_valid()]):
+            recipe_form = recipe_form.save(False)
+            recipe_form.author = request.user
+            ing = ingredients_form.save()
+            rec_step = recipe_step_form.save()
+            rec = recipe_form.save()
+
+            ing.ingredients = ing
+            rec_step.steps = rec_step
+            rec.save()
+
+            messages.success(request, "Your profile was successfully updated!")
             return redirect('recipe_list')
+
     else:
-        form = RecipeForm()
+        messages.error(request, "Please correct the error below.")
+        recipe_form = RecipeForm()
+        ingredients_form = IngredientsForm()
+        recipe_step_form = RecipeStepForm()
+
     context = {
-        'form': form,
+        'recipe_form': recipe_form,
+        'ingredients_form': ingredients_form,
+        'recipe_step_form': recipe_step_form,
 
     }
     return render(request, 'recipes/create.html', context)
+
+
+# # create recipe
+# @login_required(login_url='/accounts/login/')
+# def create_recipe(request):
+#     if request.method == 'POST':
+#         form  = RecipeForm(request.POST)
+#         if form.is_valid():
+#             recipe = form.save(False)
+#             recipe.author = request.user
+#             recipe.save()
+#             return redirect('recipe_list')
+#     else:
+#         form = RecipeForm()
+
+#     context = {
+#         'form': form,
+#     }
+
+#     return render(request, 'recipes/create.html', context)
+
 
 # USER SPECIFIC FILTERED LIST
 @login_required
@@ -52,7 +90,7 @@ def show_recipe(request, id):
     return render(request, 'recipes/detail.html', context)
 
 
-#edit/ update
+#edit/ update recipe
 def edit_recipe(request, id):
     recipe = get_object_or_404(Recipe, id=id)
     if request.method == 'POST':
@@ -68,14 +106,25 @@ def edit_recipe(request, id):
         }
     return render(request, 'recipes/edit.html', context)
 
-#delete
+
+# #edit/ update recipe
+# def edit_recipe(request, id):
+#     recipe = get_object_or_404(Recipe, id=id)
+#     if request.method == 'POST':
+#         form = RecipeForm(request.POST, instance = recipe)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('show_recipe', id=id)
+#     else:
+#         form = RecipeForm(instance = recipe)
+#     context = {
+#         'recipe': recipe,
+#         'form': form,
+#         }
+#     return render(request, 'recipes/edit.html', context)
+
+#delete recipe
 def delete_recipe(request, id):
     query = Recipe.objects.get(id=id)
     query.delete()
     return HttpResponse("Recipe has been deleted!")
-
-# alternative delete:
-# def delete_recipe(request, id):
-#     recipe = get_object_or_404(Recipe, id=id)
-#     recipe.delete()
-#     return redirect('recipe_list')
