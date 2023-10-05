36fef2a91d1a6aaabce04c05f5832ecb5dff0676
pizzawarrior <i.norstad@gmail.com>
2023-10-05 11:38:00 -0700

    first docker phase complete

--- scrumptious/settings.py
+++ scrumptious/settings.py
@@ -80,7 +80,8 @@ WSGI_APPLICATION = 'scrumptious.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'data/db.sqlite3',
     }
 }
 
